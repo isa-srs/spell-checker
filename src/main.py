68f61a110c9f1ac8a87b08af655a03dbdb6dc612
isabel <isabel.suorsa@helsinki.fi>
2025-02-16 00:11:03 +0200

    App cli updated

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 import damerau_levenshtein as dl
+import trie
 
 def main():
     """Starts and runs the program.
     """
 
+    print("Ladataan sanastoa...")
+    trie.insert_vocab("src/data/sanasto.txt")
+    print("Sanasto ladattu!")
+
     while True:
-        typo = input("Syötä väärinkirjoitettu sana: ")
-        if typo == "":
+        given_word = input("Syötä sana: ")
+        if given_word == "":
             break
-
-        correct_spelling = input("Syötä oikeinkirjoitettu sana: ")
-
-        distance = dl.damerau_levenshtein(typo, correct_spelling)
-
-        print(f'Sanojen etäisyys on {distance}.')
+        elif trie.is_correctly_spelled(given_word):
+            print("Sana on oikein kirjoitettu.")
+        else:
+            gd = trie.get_distances(given_word)
+            print(f'Tarkoititko {gd[0]}? Sanojen etäisyys on {gd[1]}.')
         print()
 
 if __name__ == "__main__":
