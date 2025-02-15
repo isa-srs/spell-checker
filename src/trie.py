12de850f2af9015f7464e235eef408be97b88bf6
isabel <isabel.suorsa@helsinki.fi>
2025-02-16 00:08:37 +0200

    Larger trie-module

--- src/trie.py
+++ src/trie.py
@@ -1,8 +1,14 @@
+# TODO: refaktoroi
+
+import damerau_levenshtein as dl
+
+
 class Node:
     def __init__(self):
         self.children = {}
         self.is_terminal = False
     
+root = Node()
 
 def insert_word(root, word):
     current = root
@@ -14,6 +20,12 @@ def insert_word(root, word):
         current = current.children[char]
     current.is_terminal = True
 
+def insert_vocab(file):
+    with open(file) as f:
+        content = f.read().splitlines()
+        for word in content:
+            insert_word(root, word)
+
 def search_word(root, word):
     current = root
     for char in word:
@@ -22,18 +34,26 @@ def search_word(root, word):
         current = current.children[char]
     return current.is_terminal
 
-# test
-root = Node()
-with open("src/data/sanasto.txt") as f:
-    content = f.read().splitlines()
-    for word in content:
-        insert_word(root, word)
+def is_correctly_spelled(word):
+    if search_word(root, word):
+        return True
+    return False
+
+def get_distances(typo):
+    min_distance = 100
+    closest = ""
+    with open("src/data/sanasto.txt") as f:
+        content = f.read().splitlines()
+        for word in content:
+            distance = dl.damerau_levenshtein(typo, word)
+            if distance < min_distance:
+                min_distance = distance
+                closest = word
+    return (closest, min_distance)
+
     
 
-search_words = ["koira", "kala", "aakkonen"]
-for word in search_words:
-    print(f"Sana: {word}")
-    if search_word(root, word):
-        print("Löytyy")
-    else:
-        print("Ei löydy")
+if __name__ == "__main__":
+    insert_vocab("src/data/sanasto.txt")
+    print(is_correctly_spelled("koira"))
+    print(get_distances("koimoas"))
