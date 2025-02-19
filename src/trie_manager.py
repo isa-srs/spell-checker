1a5b1c50321e4f44236417e3d7c07ad202a91a70
isabel <isabel.suorsa@helsinki.fi>
2025-02-19 15:57:30 +0200

    created trie_manager for handling trie commands

---
+++ src/trie_manager.py
@@ -0,0 +1,27 @@
+from trie import Trie
+import damerau_levenshtein as dl
+
+class TrieManager:
+    def __init__(self):
+        self.trie = Trie()
+
+    def insert_vocabulary(self, file):
+        with open(file) as file:
+            content = file.read().splitlines()
+            for word in content:
+                self.trie.insert_word(word)
+    
+    def search_word(self, word):
+        return self.trie.search_word(word)
+    
+    def get_distances(self, typo):
+        min_distance = 100
+        closest = ""
+        with open("src/data/sanasto.txt") as f:
+            content = f.read().splitlines()
+            for word in content:
+                distance = dl.damerau_levenshtein(typo, word)
+                if distance < min_distance:
+                    min_distance = distance
+                    closest = word
+        return (closest, min_distance)
