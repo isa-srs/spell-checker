1c05c4789f3de357d632ec1b2d4af02a788bfef5
isabel <isabel.suorsa@helsinki.fi>
2025-02-19 15:56:54 +0200

    refactored trie.py

--- src/trie.py
+++ src/trie.py
@@ -1,59 +1,25 @@
-# TODO: refaktoroi
-
-import damerau_levenshtein as dl
-
-
 class Node:
     def __init__(self):
         self.children = {}
         self.is_terminal = False
-    
-root = Node()
 
-def insert_word(root, word):
-    current = root
-    for char in word:
-        if char not in current.children:
-            new = Node()
-            current.children[char] = new
+class Trie:
+    def __init__(self):
+        self.root = Node()
 
-        current = current.children[char]
-    current.is_terminal = True
+    def insert_word(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = Node()
 
-def insert_vocab(file):
-    with open(file) as f:
-        content = f.read().splitlines()
-        for word in content:
-            insert_word(root, word)
+            current = current.children[char]
+        current.is_terminal = True
 
-def search_word(root, word):
-    current = root
-    for char in word:
-        if char not in current.children:
-            return False
-        current = current.children[char]
-    return current.is_terminal
-
-def is_correctly_spelled(word):
-    if search_word(root, word):
-        return True
-    return False
-
-def get_distances(typo):
-    min_distance = 100
-    closest = ""
-    with open("src/data/sanasto.txt") as f:
-        content = f.read().splitlines()
-        for word in content:
-            distance = dl.damerau_levenshtein(typo, word)
-            if distance < min_distance:
-                min_distance = distance
-                closest = word
-    return (closest, min_distance)
-
-    
-
-if __name__ == "__main__":
-    insert_vocab("src/data/sanasto.txt")
-    print(is_correctly_spelled("koira"))
-    print(get_distances("koimoas"))
+    def search_word(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return current.is_terminal
