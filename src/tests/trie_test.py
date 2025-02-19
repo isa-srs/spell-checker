1ca0b2fe91bbac20f8e369beb437a974c77e0b42
isabel <isabel.suorsa@helsinki.fi>
2025-02-19 17:39:55 +0200

    tested inserting words to trie!

---
+++ src/tests/trie_test.py
@@ -0,0 +1,16 @@
+import unittest
+from trie import Trie
+
+FILE = "src/data/test_vocabulary.txt"
+
+class TestTrieStructure(unittest.TestCase):
+    def setUp(self):
+        self.trie = Trie()
+
+    def test_insert_word(self):
+        self.trie.insert_word("aakkonen")
+
+        self.assertEqual(self.trie.search_word("aakkonen"), True)
+        self.assertEqual(self.trie.search_word("apina"), False)
+
+
